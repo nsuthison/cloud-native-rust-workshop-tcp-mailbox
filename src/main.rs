3978382bfad565408909f3a64ffe1fa3d8197c9a
Natechawin <natechawin@gmail.com>
2021-05-03 21:40:19 +0700

    add simple tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listerner = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for connection_attempt in listerner.incoming() {
+        match connection_attempt {
+            Ok(stream) => {
+                handle_client(stream)
+            },
+            Err(e) => {
+                eprintln!("Error connecting: {}", e)
+            }
+        }
+    }
+}
+
+fn handle_client(stream: TcpStream) -> () {
+    println!("Client connected!!");
+
+    drop(stream);
 }
