54c2ff1ad5571f4dd9717123d2c27d05ab431732
Natechawin <natechawin@gmail.com>
2021-05-03 22:09:42 +0700

    add request enum and request parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use std::io::{BufRead, BufReader};
 use std::net::{TcpListener, TcpStream};
+
+#[derive(Debug, Eq, PartialEq)]
+enum Request {
+    Publish(String),
+    Retrieve
+}
 
 fn main() {
     let listerner = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -7,7 +14,7 @@ fn main() {
         match connection_attempt {
             Ok(stream) => {
                 handle_client(stream)
-            },
+            }
             Err(e) => {
                 eprintln!("Error connecting: {}", e)
             }
@@ -15,8 +22,37 @@ fn main() {
     }
 }
 
-fn handle_client(stream: TcpStream) -> () {
-    println!("Client connected!!");
+fn handle_client(stream: TcpStream) {
+    let line = read_line(&stream);
+    let request = parse_request(line);
 
-    drop(stream);
+    match request {
+        Request::Publish(msg) => {
+            eprintln!("publishing message")
+        }
+        Request::Retrieve => {
+            eprintln!("retrieving message")
+        }
+    }
+
+    println!("Client connected!");
+}
+
+fn parse_request(line: String) -> Request {
+    let trimmed = line.trim_end();
+
+    if trimmed == "" {
+        Request::Retrieve
+    } else {
+        Request::Publish(String::from(trimmed))
+    }
+}
+
+fn read_line(stream: &TcpStream) -> String {
+    let mut buffered_reader = BufReader::new(stream);
+
+    let mut buf = String::new();
+    buffered_reader.read_line(&mut buf).unwrap();
+
+    buf
 }
